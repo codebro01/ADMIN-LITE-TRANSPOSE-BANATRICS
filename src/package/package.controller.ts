1bb1d77a4a9bf9b8554ffec19e4052d6a2267ee6
codebro <samsondamilola.99@gmail.com>
2026-01-14 09:15:22 +0100

    added payment graphs query

--- src/package/package.controller.ts
+++ src/package/package.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
 import { PackageService } from './package.service';
 import { CreatePackageDto } from './dto/create-package.dto';
 import { UpdatePackageDto } from './dto/update-package.dto';
@@ -6,6 +18,7 @@ import { JwtAuthGuard } from '@src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from '@src/auth/guards/roles.guard';
 import { Roles } from '@src/auth/decorators/roles.decorators';
 import type { Request } from '@src/types';
+import { ApiCookieAuth, ApiOperation } from '@nestjs/swagger';
 
 @Controller('packages')
 export class PackageController {
@@ -14,14 +27,26 @@ export class PackageController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('admin')
   @Post()
+  @ApiCookieAuth('access_token')
+  @ApiOperation({
+    summary: 'Creates a package type',
+    description: 'Create package type',
+  })
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createPackageDto: CreatePackageDto, @Req() req: Request) {
-    const {id: userId} = req.user;
+    const { id: userId } = req.user;
     return this.packageService.create(createPackageDto, userId);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('admin')
   @Get()
+  @ApiCookieAuth('access_token')
+  @ApiOperation({
+    summary: 'Finds all packages',
+    description: 'Finds all packages',
+  })
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.packageService.findAll();
   }
@@ -29,6 +54,13 @@ export class PackageController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('admin')
   @Get(':id')
+  @ApiCookieAuth('access_token')
+  @ApiOperation({
+    summary: 'Gets information of a single package',
+    description:
+      'Gets information of a single package by providing the package id',
+  })
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
     return this.packageService.findOne(id);
   }
@@ -36,6 +68,12 @@ export class PackageController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('admin')
   @Patch(':id')
+  @ApiCookieAuth('access_token')
+  @ApiOperation({
+    summary: 'Updates a package',
+    description: 'Updates a package by providing the package id',
+  })
+  @HttpCode(HttpStatus.OK)
   update(@Param('id') id: string, @Body() updatePackageDto: UpdatePackageDto) {
     return this.packageService.update(id, updatePackageDto);
   }
@@ -43,6 +81,12 @@ export class PackageController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles('admin')
   @Delete(':id')
+  @ApiCookieAuth('access_token')
+  @ApiOperation({
+    summary: 'Deletes a package',
+    description: 'Deletes a package forever',
+  })
+  @HttpCode(HttpStatus.OK)
   remove(@Param('id') id: string) {
     return this.packageService.remove(id);
   }
--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -7,39 +7,12 @@ import {
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
-import {
-  generateSecureInvoiceId,
-  generateSecureRef,
-  PaymentRepository,
-} from '@src/payment/repository/payment.repository';
+import { PaymentRepository } from '@src/payment/repository/payment.repository';
 import { UserRepository } from '@src/users/repository/user.repository';
-import crypto from 'crypto';
-import { InitializePaymentDto } from '@src/payment/dto/initializePaymentDto';
-import { MakePaymentForCampaignDto } from '@src/payment/dto/makePaymentForCampaignDto';
 import { CampaignRepository } from '@src/campaign/repository/campaign.repository';
-import {
-  CategoryType,
-  StatusType,
-  VariantType,
-} from '@src/notification/dto/createNotificationDto';
+
 import { NotificationService } from '@src/notification/notification.service';
 import { EarningRepository } from '@src/earning/repository/earning.repository';
-import { PaymentStatusType } from '@src/payment/dto/paystackMetadataDto';
-interface VerifyPaymentResponse {
-  status: boolean;
-  message: string;
-  data: {
-    id: number;
-    status: string;
-    reference: string;
-    amount: number;
-    gateway_response: string;
-    paid_at: string;
-    customer: {
-      email: string;
-    };
-  };
-}
 
 @Injectable()
 export class PaymentService {
@@ -68,311 +41,7 @@ export class PaymentService {
     };
   }
 
-  async initializePayment(data: InitializePaymentDto & {email: string, userId: string}) {
-    try {
-      const response = await firstValueFrom(
-        this.httpService.post(
-          `${this.baseUrl}/transaction/initialize`,
-          {
-            email: data.email,
-            amount: data.amount,
-            reference: generateSecureRef(),
-            callback_url: data.callback_url,
-            metadata: {
-              amountInNaira: data.amount/100, 
-              userId: data.userId,
-              invoiceId: generateSecureInvoiceId(),
-              dateInitiated: new Date().toISOString(),
-            },
-          },
-          { headers: this.getHeaders() },
-        ),
-      );
-
-      return response.data;
-    } catch (error) {
-      console.log(error);
-      throw new HttpException(
-        error.response?.data?.message || 'Failed to initialize payment',
-        error.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
-
-  //! verify payments
-
-  async verifyPayment(reference: string): Promise<VerifyPaymentResponse> {
-    try {
-      const response = await firstValueFrom(
-        this.httpService.get(
-          `${this.baseUrl}/transaction/verify/${reference}`,
-          { headers: this.getHeaders() },
-        ),
-      );
-
-      return response.data;
-    } catch (error) {
-      throw new HttpException(
-        error.response?.data?.message || 'Failed to verify payment',
-        error.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-  }
-
-  //! verify webhook signatures
-
-  verifyWebhookSignature(payload: string, signature: string): boolean {
-    const hash = crypto
-      .createHmac('sha512', this.secretKey)
-      .update(payload)
-      .digest('hex');
-
-    return hash === signature;
-  }
-
-  // ! handle post verify webhooks
-
-  async postVerifyWebhookSignatures(event: any) {
-    try {
-      const { reference, createdAt, amount } = event.data;
-      const { channel } = event.data.authorization || {};
-      const { userId, amountInNaira, invoiceId, dateInitiated } =
-        event.data.metadata || {};
-      const { recipient_code } = event.data.recipient;
-      // const {account_number, account_name, bank_name, bank_code} = event.data.recipient.details
-
-      switch (event.event) {
-        case 'charge.success': {
-          const existingPayment =
-            await this.paymentRepository.findByReference(reference);
-
-          if (existingPayment && existingPayment.paymentStatus === PaymentStatusType.SUCCESS) {
-            return 'already processed';
-          }
-
-          await this.paymentRepository.executeInTransaction(async (trx) => {
-            await this.paymentRepository.savePayment(
-              {
-                amount: amountInNaira,
-                invoiceId,
-                dateInitiated,
-                paymentStatus: PaymentStatusType.SUCCESS,
-                paymentMethod: channel,
-                reference,
-                transactionType: 'deposit',
-              },
-              userId,
-              trx,
-            );
-
-            await this.paymentRepository.updateBalance(
-              { amount: amountInNaira },
-              userId,
-              trx,
-            );
-          });
-
-          await this.notificationService.createNotification(
-            {
-              title: `Your deposit of ${amountInNaira} is successfull`,
-              message: `You have successfully deposited ${amountInNaira} through ${channel} `,
-              variant: VariantType.SUCCESS,
-              category: CategoryType.PAYMENT,
-              priority: '',
-              status: StatusType.UNREAD,
-            },
-            userId,
-          );
-
-          break;
-        }
-        case 'charge.failed': {
-          await this.paymentRepository.executeInTransaction(async (trx) => {
-            await this.paymentRepository.savePayment(
-              {
-                amount: amountInNaira,
-                invoiceId,
-                dateInitiated,
-                paymentStatus: PaymentStatusType.FAILED,
-                paymentMethod: channel,
-                reference,
-                transactionType: 'deposit',
-              },
-              userId,
-              trx,
-            );
-          });
-
-          await this.notificationService.createNotification(
-            {
-              title: `Your deposit of ${amountInNaira}  failed`,
-              message: `Your deposited of ${amountInNaira} through ${channel} may have failed due to some reasons, please try again `,
-              variant: VariantType.DANGER,
-              category: CategoryType.PAYMENT,
-              priority: '',
-              status: StatusType.UNREAD,
-            },
-            userId,
-          );
-          break;
-        }
-
-        case 'charge.pending': {
-          await this.paymentRepository.executeInTransaction(async (trx) => {
-            await this.paymentRepository.savePayment(
-              {
-                amount: amountInNaira,
-                invoiceId,
-                dateInitiated,
-                paymentStatus: PaymentStatusType.PENDING,
-                paymentMethod: channel,
-                reference,
-                transactionType: 'deposit',
-              },
-              userId,
-              trx,
-            );
-          });
-
-          await this.notificationService.createNotification(
-            {
-              title: `Your deposit of ${amountInNaira} is pending`,
-              message: `Your deposited of ${amountInNaira} through ${channel} is still pending, please kindly wait while the payment for the payment to be comfirmed `,
-              variant: VariantType.INFO,
-              category: CategoryType.PAYMENT,
-              priority: '',
-              status: StatusType.UNREAD,
-            },
-            userId,
-          );
-          break;
-        }
-
-        case 'refund.processed': {
-          await this.paymentRepository.executeInTransaction(async (trx) => {
-            await this.paymentRepository.updatePaymentStatus(
-              { reference, status: PaymentStatusType.REVERSED},
-              userId,
-              trx,
-            );
-
-            await this.paymentRepository.updateBalance(
-              { amount: -amountInNaira },
-              userId,
-              trx,
-            );
-          });
-
-          await this.notificationService.createNotification(
-            {
-              title: `Refund of ${amountInNaira} is proccessing`,
-              message: `Your refund of ${amountInNaira} is processing, please wait while it completes `,
-              variant: VariantType.INFO,
-              category: CategoryType.PAYMENT,
-              priority: '',
-              status: StatusType.UNREAD,
-            },
-            userId,
-          );
-
-          break;
-        }
-
-        case 'transfer.success': {
-          await this.paymentRepository.executeInTransaction(async (trx) => {
-            await this.earningRepository.createEarnings(
-              {
-                amount: amount,
-                reference,
-                dateInitiated: createdAt,
-                recipientCode: recipient_code,
-                paymentStatus: PaymentStatusType.SUCCESS,
-                paymentMethod: 'transfer',
-              },
-              trx,
-            );
-          });
-          await this.notificationService.createNotification(
-            {
-              title: `Your withdrawal of ${amount} is successful`,
-              message: `Kindly wait a few seconds to receive the funds in your connected bank.`,
-              variant: VariantType.INFO,
-              category: CategoryType.PAYMENT,
-              priority: '',
-              status: StatusType.UNREAD,
-            },
-            userId,
-          );
-
-          break;
-        }
-        case 'transfer.failed': {
-          await this.paymentRepository.executeInTransaction(async (trx) => {
-            await this.earningRepository.createEarnings(
-              {
-                amount: amount,
-                reference,
-                dateInitiated: createdAt,
-                recipientCode: recipient_code,
-                paymentStatus: PaymentStatusType.FAILED,
-                paymentMethod: 'transfer',
-              },
-              trx,
-            );
-          });
-          await this.notificationService.createNotification(
-            {
-              title: `Your withdrawal of ${amount} is processing!!!`,
-              message: `Kindly wait some minutes while we proccess the withdrawal of your funds`,
-              variant: VariantType.SUCCESS,
-              category: CategoryType.PAYMENT,
-              priority: '',
-              status: StatusType.UNREAD,
-            },
-            userId,
-          );
-          break;
-        }
-        case 'transfer.reversed': {
-          await this.paymentRepository.executeInTransaction(async (trx) => {
-            await this.earningRepository.createEarnings(
-              {
-                amount: amount,
-                reference,
-                dateInitiated: createdAt,
-                recipientCode: recipient_code,
-                paymentStatus: PaymentStatusType.REVERSED,
-                paymentMethod: 'transfer',
-              },
-              trx,
-            );
-          });
-          await this.notificationService.createNotification(
-            {
-              title: `Your withdrawal of ${amount} is been processed`,
-              message: `Kindly wait some minutes while  we process the withdrawal of your funds`,
-              variant: VariantType.INFO,
-              category: CategoryType.PAYMENT,
-              priority: '',
-              status: StatusType.UNREAD,
-            },
-            userId,
-          );
-          break;
-        }
-
-        default:
-      }
-
-      return { status: 'success' };
-    } catch (error) {
-      console.error('Webhook processing error:', error);
-      return { error: error.message };
-    }
-  }
-
-  //! admin get transaction details
-
+  //! paystack query
   async getTransaction(id: number) {
     try {
       const response = await firstValueFrom(
@@ -389,8 +58,6 @@ export class PaymentService {
       );
     }
   }
-
-  //! admin list all transactions details
 
   async listAllTransactions(params?: { perPage?: number; page?: number }) {
     try {
@@ -413,68 +80,36 @@ export class PaymentService {
     }
   }
 
-  async makePaymentForCampaign(
-    data: MakePaymentForCampaignDto,
-    userId: string,
-  ) {
-    const { campaignId } = data;
-    try {
-      const result = await this.paymentRepository.moveMoneyFromBalanceToPending(
-        { campaignId },
-        userId,
-      );
+  //! Db calls
 
-      return result;
-    } catch (error) {
-      console.error('error', error.message);
-      throw new HttpException(
-        error.response?.data?.message ||
-          error?.message ||
-          'Failed to list transactions',
-        error.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  async listAllWithdrawals() {
+    return await this.paymentRepository.listAllWithdrawals();
   }
-  async finalizePaymentForCampaign(
-    data: MakePaymentForCampaignDto,
-    userId: string,
-  ) {
-    const { campaignId } = data;
-    try {
-      const result =
-        await this.paymentRepository.moveMoneyFromPendingToTotalAmountSpent(
-          { campaignId },
-          userId,
-        );
-
-      return result;
-    } catch (error) {
-      console.error('error', error.message);
-      throw new HttpException(
-        error.response?.data?.message ||
-          error?.message ||
-          'Failed to list transactions',
-        error.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+  async listAllPayments() {
+    return await this.paymentRepository.listAllPayments();
   }
-  async listTransactions(userId: string) {
-    try {
-      const result = await this.paymentRepository.listTransactions(userId);
-
-      return result;
-    } catch (error) {
-      console.error('error', error.message);
-      throw new Error(error);
-    }
+  async totalMonthlyIncomeGraph() {
+    return await this.paymentRepository.totalMonthlyIncomeGraph();
   }
-  async paymentDashboard(userId: string) {
-    try {
-      const result = await this.paymentRepository.paymentDashboard(userId);
-      return result;
-    } catch (error) {
-      console.error('error', error.message);
-      throw new Error(error);
-    }
+  async totalWeeklyIncomeGraph() {
+    return await this.paymentRepository.totalWeeklyIncomeGraph();
+  }
+  async totalYearlyIncomeGraph() {
+    return await this.paymentRepository.totalYearlyIncomeGraph();
+  }
+  async totalDriverPayoutsWeekly() {
+    return await this.paymentRepository.totalDriverPayoutsWeekly();
+  }
+  async totalDriverPayoutsMonthly() {
+    return await this.paymentRepository.totalDriverPayoutsMonthly();
+  }
+  async totalDriverPayoutsYearly() {
+    return await this.paymentRepository.totalDriverPayoutsYearly();
+  }
+  async netProfitMonthly() {
+    return await this.paymentRepository.netProfitMonthly();
+  }
+  async netProfitYearly() {
+    return await this.paymentRepository.netProfitYearly();
   }
 }
